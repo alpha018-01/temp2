48a78b3664f9a1b4bf3dbd8122063bc5dde0e8ae
alpha018-01 <152808711+alpha018-01@users.noreply.github.com>
2025-03-28 15:47:53 +0530

    Add files via upload

---
+++ rough.py
@@ -0,0 +1,38 @@
+import shutil
+
+from flask import Flask, after_this_request, send_file, render_template
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+def getMpFour(link):
+    
+    yt = YouTube(link , on_progress_callback = on_progress)
+    print(yt.title)
+    
+    video = yt.streams.get_highest_resolution()
+    filePath = video.download('./video')
+    return filePath
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index() :
+    return render_template('index.html')
+
+@app.route("/<path:arg>")
+def getVideo(arg) :
+    file_path = getMpFour(arg)
+    response = send_file(file_path, as_attachment=True)
+    return response 
+
+@app.route('/download')
+def download() :
+    return render_template('download.html')
+
+@app.route('/')
+def main() :
+    return render_template('main.html')
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
